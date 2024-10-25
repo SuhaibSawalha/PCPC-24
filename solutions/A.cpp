b7a819ef7e6da62d8d0bcc158156ef0637e2aa58
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2024-10-25 19:14:58 +0300

    Create A.cpp

---
+++ solutions/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main () {
+
+  #ifndef ONLINE_JUDGE
+    freopen("Satoru","r",stdin);
+  #endif
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int x, y, n;
+		cin >> x >> y >> n;
+		cout << min(x * n, (n / 3) * y + (n % 3) * x) << "\n";
+	} 
+
+  return 0;
+}
