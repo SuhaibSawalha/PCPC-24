938b4fef1edebe9efb8fd7f0365a16484b9ae1c1
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2024-10-25 19:15:34 +0300

    Create C.cpp

---
+++ solutions/C.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main () {
+
+  #ifndef ONLINE_JUDGE
+    freopen("Satoru","r",stdin);
+  #endif
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+	int n;
+	string s;
+	cin >> n >> s;
+	long long p = 1, ans = 0;
+	for (int i = n - 1; ~i; --i) {
+		if (s[i] == '1') {
+			ans += p;
+		}
+		p *= 2;
+	}
+	cout << ans;
+
+  return 0;
+}
