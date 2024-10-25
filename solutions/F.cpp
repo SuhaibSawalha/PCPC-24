d7d84ffea34d09c6dc2fd94b1a4ccbf5ddee1e41
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2024-10-25 19:16:25 +0300

    Create F.cpp

---
+++ solutions/F.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+int main () {
+
+  #ifndef ONLINE_JUDGE
+    freopen("Satoru","r",stdin);
+  #endif
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	cout << n - 1; 
+
+  return 0;
+}
