56043c50ffd5705d2a6bc7b6dbe29a3973dc4e2c
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2024-10-25 23:55:18 +0300

    Update B.cpp

--- solutions/B.cpp
+++ solutions/B.cpp
@@ -10,27 +10,27 @@ int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-	int tt;
-	cin >> tt;
-	while (tt--) {
-		int n;
-		cin >> n;
-		int e = 0, o = 0;
-		long long ans = 0;
-		for (int i = 0; i < n; ++i) {
-			int x;
-			cin >> x;
-			if (x % 2) {
-				ans += e;
-				o += 1;
-			}
-			else {
-				ans += o;
-				e += 1;
-			}
-		}
-		cout << ans << "\n";
-	} 
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n;
+    cin >> n;
+    int e = 0, o = 0;
+    long long ans = 0;
+    for (int i = 0; i < n; ++i) {
+      int x;
+      cin >> x;
+      if (x % 2) {
+        ans += e;
+        o += 1;
+      }
+      else {
+        ans += o;
+        e += 1;
+      }
+    }
+    cout << ans << "\n";
+  } 
 
   return 0;
 }
