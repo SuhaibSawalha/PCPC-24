7f80ec5d8907bc72108642b80ba721b414c95b63
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2024-10-25 23:57:51 +0300

    Update L.cpp

--- solutions/L.cpp
+++ solutions/L.cpp
@@ -10,23 +10,23 @@ int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-	int n, x;
-	cin >> n >> x;
-	vector<int> a(n);
-	for (int i = 0; i < n; ++i) {
-		cin >> a[i];
-	} 
-	sort(a.begin(), a.end());
-	int l = 0;
-	long long ans = 0;
-	for (int r = 0; r < n; ++r) {
-		while (a[r] - a[l] > x) {
-			++l;
-		}
-		int d = r - l;
-		ans += d * 1LL * (d - 1) / 2;
-	}
-	cout << ans;
+  int n, x;
+  cin >> n >> x;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  } 
+  sort(a.begin(), a.end());
+  int l = 0;
+  long long ans = 0;
+  for (int r = 0; r < n; ++r) {
+    while (a[r] - a[l] > x) {
+      ++l;
+    }
+    int d = r - l;
+    ans += d * 1LL * (d - 1) / 2;
+  }
+  cout << ans;
 
   return 0;
 }
