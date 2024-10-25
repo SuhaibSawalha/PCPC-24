4318ab9400f152b495d0830f15c8478ed1b0a3f0
Suhaib Sawalha <68230953+SuhaibSawalha@users.noreply.github.com>
2024-10-25 23:57:36 +0300

    Update K.cpp

--- solutions/K.cpp
+++ solutions/K.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 const int mod = 1e9 + 7;
 int add (int a, int b) {
-	return (a + b) % mod;
+  return (a + b) % mod;
 }
 int mul (int a, int b) {
-	return (a * 1LL * b) % mod;
+  return (a * 1LL * b) % mod;
 }
 int sub (int a, int b) {
-	return ((a - b) % mod + mod) % mod;
+  return ((a - b) % mod + mod) % mod;
 }
 
 int fastpow (int v, long long p) {
-	int res = 1;
-	while (p) {
-		if (p % 2) {
-			res = mul(res, v);
-		}
-		v = mul(v, v);
-		p /= 2;
-	}
-	return res;
+  int res = 1;
+  while (p) {
+    if (p % 2) {
+      res = mul(res, v);
+    }
+    v = mul(v, v);
+    p /= 2;
+  }
+  return res;
 }
 int Inv (int n) {
-	return fastpow(n, mod - 2);
+  return fastpow(n, mod - 2);
 }
     
 int main () {
@@ -36,23 +36,23 @@ int main () {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-	int tt;
-	cin >> tt;
-	while (tt--) {
-		string s;
-		cin >> s;
-		int n = s.size(), ans = 1, inv = Inv(26);
-		for (int i = 0; i < n / 2; ++i) {
-			if (s[i] == '?' || s[n - i - 1] == '?') {
-				ans = mul(ans, inv);
-			}
-			else if (s[i] != s[n - i - 1]) {
-				ans = 0;
-				break;
-			}
-		}
-		cout << ans << "\n";
-	} 
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    string s;
+    cin >> s;
+    int n = s.size(), ans = 1, inv = Inv(26);
+    for (int i = 0; i < n / 2; ++i) {
+      if (s[i] == '?' || s[n - i - 1] == '?') {
+        ans = mul(ans, inv);
+      }
+      else if (s[i] != s[n - i - 1]) {
+        ans = 0;
+        break;
+      }
+    }
+    cout << ans << "\n";
+  } 
 
   return 0;
 }
